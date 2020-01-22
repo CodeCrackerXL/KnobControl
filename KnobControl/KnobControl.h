caac55c9cda42065cb6e60944507c26705cab383
Xiao Liang <liangxiao0215@live.com>
2020-01-21 21:14:47 -0800

    Initial commit

---
+++ KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by 梁骁 on 1/21/20.
+//  Copyright © 2020 梁骁. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # KnobControl
 An iOS framework
+
+
